0da2c5f7b1d1ef008e574243eeb77436d595c7a9
Rahul <rahul17a1997@gmail.com>
2020-07-15 12:58:19 +0530

    change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  # skip_before_action :authorize
+  skip_before_action :authorize
 
   def index
     @questions = Question.published.includes(:user, {answers: [:user, {comments: [:user]} ]}, {comments: [:user]} ).paginate(page: params[:page], per_page: ENV['pagination_size'].to_i).order(updated_at: :desc)
